a456aba55847d6a0e8899616aeed08f6bf820f36
Sahil Chawla <99795433+chawla971@users.noreply.github.com>
2025-08-18 12:59:04 -0700

    Fix for render

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',             // generates the static 'out' folder on build
+  images: { unoptimized: true } // needed if you use next/image in static export
+  // trailingSlash: true,        // optional if you have static hosting quirks
 };
 
 export default nextConfig;
